6611073ba4d355fc335cd26fde78bff29d48d2e1
Alexander Roque <alexroque91@gmail.com>
2018-12-05 23:44:48 -0200

    More stuff

--- dsnet.py
+++ dsnet.py
@@ -14,7 +14,7 @@ import utils as utils
 
 #Constants
 img_size = 64
-epoch_num = 5000
+epoch_num = 1000
 
 #PATHS
 ROOT_PATH = "/traffic"
@@ -62,6 +62,7 @@ with graph.as_default():
 
     layer1 = tf.contrib.layers.fully_connected(conv_flat, 250, tf.nn.tanh)
     logits = tf.contrib.layers.fully_connected(layer1, 62, tf.nn.tanh)
+    softmax = tf.nn.softmax(logits)
 
     predicted_labels = tf.argmax(logits, 1)
 
@@ -82,29 +83,31 @@ images_a, labels_a = shuffle(images_a, labels_a, random_state=0)
 tempo_inicial = time()
 for i in range(epoch_num):
     _, loss_value = session.run([train, loss], feed_dict={images_ph: images_a, labels_ph: labels_a})
-    if i % 50 == 49:
-        predicted = session.run([predicted_labels], feed_dict={images_ph: test_images_resized})[0]
-        match_count = sum([int(y == y_) for y, y_ in zip(test_labels, predicted)])
-        accuracy = match_count / len(test_labels)
-        predicted = session.run([predicted_labels], feed_dict={images_ph: images_resized})[0]
-        match_count = sum([int(y == y_) for y, y_ in zip(labels, predicted)])
-        accuracy_train = match_count / len(labels)
-        print("Loss: {:.3f}      Accuracy: {:.3f}      TAccuracy: {:.3f}".format(loss_value, accuracy, accuracy_train))
-        csv_array[0][int(i/50)+4] = i
-        csv_array[1][int(i/50)+4] = loss_value
-        csv_array[2][int(i/50)+4] = accuracy
-        csv_array[3][int(i/50)+4] = accuracy_train
-        if i % 1000 == 999:
-            predicted = session.run([predicted_labels], feed_dict={images_ph: test_images_resized})[0]
-            prediction_csv = np.empty([2, len(test_labels)])
-            prediction_csv[0] = test_labels
-            prediction_csv[1] = predicted
-            np.savetxt("predictions_" + str(i) + "_test.csv", prediction_csv.T, delimiter=",", fmt='%10.5f')
-            predicted_train = session.run([predicted_labels], feed_dict={images_ph: images_resized})[0]
-            prediction_train_csv = np.empty([2, len(labels)])
-            prediction_train_csv[0] = labels
-            prediction_train_csv[1] = predicted_train
-            np.savetxt("predictions_" + str(i) + "_train.csv", prediction_train_csv.T, delimiter=",", fmt='%10.5f')
+    if i % 100 == 99:
+    	print("Loss: {:.3f}".format(loss_value))
+    # if i % 50 == 49:
+        # predicted = session.run([predicted_labels], feed_dict={images_ph: test_images_resized})[0]
+        # match_count = sum([int(y == y_) for y, y_ in zip(test_labels, predicted)])
+        # accuracy = match_count / len(test_labels)
+        # predicted = session.run([predicted_labels], feed_dict={images_ph: images_resized})[0]
+        # match_count = sum([int(y == y_) for y, y_ in zip(labels, predicted)])
+        # accuracy_train = match_count / len(labels)
+        # print("Loss: {:.3f}      Accuracy: {:.3f}      TAccuracy: {:.3f}".format(loss_value, accuracy, accuracy_train))
+        # csv_array[0][int(i/50)+4] = i
+        # csv_array[1][int(i/50)+4] = loss_value
+        # csv_array[2][int(i/50)+4] = accuracy
+        # csv_array[3][int(i/50)+4] = accuracy_train
+        # if i % 1000 == 999:
+        #     predicted = session.run([predicted_labels], feed_dict={images_ph: test_images_resized})[0]
+        #     prediction_csv = np.empty([2, len(test_labels)])
+        #     prediction_csv[0] = test_labels
+        #     prediction_csv[1] = predicted
+        #     np.savetxt("predictions4_" + str(i) + "_test.csv", prediction_csv.T, delimiter=",", fmt='%10.5f')
+        #     predicted_train = session.run([predicted_labels], feed_dict={images_ph: images_resized})[0]
+        #     prediction_train_csv = np.empty([2, len(labels)])
+        #     prediction_train_csv[0] = labels
+        #     prediction_train_csv[1] = predicted_train
+        #     np.savetxt("predictions4_" + str(i) + "_train.csv", prediction_train_csv.T, delimiter=",", fmt='%10.5f')
 tempo_final = time()
 tempo_treinamento = tempo_final - tempo_inicial
 print('Tempo de Training:', tempo_treinamento)
@@ -120,5 +123,16 @@ tempo_execucao = tempo_final - tempo_inicial
 print('Tempo de processamento:', tempo_execucao)
 csv_array[1][2] = tempo_execucao
 
-np.savetxt("dsnet3.csv", csv_array.T, delimiter=",", fmt='%10.5f')
+
+soft = session.run([softmax], feed_dict={images_ph: test_images_resized})[0]
+predicted = session.run([predicted_labels], feed_dict={images_ph: test_images_resized})[0]
+prediction_csv = np.empty([3, len(test_labels)])
+prediction_csv[0] = test_labels
+prediction_csv[1] = predicted
+for j in range(len(test_labels)):
+	prediction_csv[2][j] = soft[j][predicted[j]]
+np.savetxt("predictions_and_probabilites.csv", prediction_csv.T, delimiter=",", fmt='%10.5f')
+np.savetxt("soft.csv", soft.T, delimiter=",", fmt='%10.5f')
+
+np.savetxt("dsnet5.csv", csv_array.T, delimiter=",", fmt='%10.5f')
 session.close()
